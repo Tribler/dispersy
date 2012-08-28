e5b863c90ab10ddc1718848617a70c7af0302028
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2012-08-28 07:24:10 +0000

    better debug output
    git-svn-id: https://svn.tribler.org/dispersy/trunk@27937 001aeff7-3401-0410-a489-f7902fc005dd

--- requestcache.py
+++ requestcache.py
@@ -2,6 +2,8 @@ from random import random
 from revision import update_revision_information
 
 if __debug__:
+    def identifier_to_string(identifier):
+        return identifier.encode("HEX") if isinstance(identifier, str) else identifier
     from dprint import dprint
 
 # update version information directly from SVN
@@ -26,7 +28,7 @@ class RequestCache(object):
         while True:
             identifier = int(random() * 2**16)
             if not identifier in self._identifiers:
-                if __debug__: dprint("claiming on ", identifier, " for ", cache)
+                if __debug__: dprint("claiming on ", identifier_to_string(identifier), " for ", cache)
                 break
 
         self.set(identifier, cache)
@@ -39,7 +41,7 @@ class RequestCache(object):
         assert isinstance(cache.timeout_delay, float)
         assert cache.timeout_delay > 0.0
 
-        if __debug__: dprint("set ", identifier, " for ", cache, " (", cache.timeout_delay, "s timeout)")
+        if __debug__: dprint("set ", identifier_to_string(identifier), " for ", cache, " (", cache.timeout_delay, "s timeout)")
         self._callback.register(self._on_timeout, (identifier,), id_="requestcache-%s" % identifier, delay=cache.timeout_delay)
         self._identifiers[identifier] = cache
         cache.identifier = identifier
@@ -48,7 +50,7 @@ class RequestCache(object):
         assert isinstance(identifier, (int, long, str)), type(identifier)
         assert issubclass(cls, Cache), cls
 
-        if __debug__: dprint("cache contains ", identifier, "? ", identifier in self._identifiers)
+        if __debug__: dprint("cache contains ", identifier_to_string(identifier), "? ", identifier in self._identifiers)
         return isinstance(self._identifiers.get(identifier), cls)
 
     def get(self, identifier, cls):
@@ -67,7 +69,7 @@ class RequestCache(object):
         if cache and isinstance(cache, cls):
             assert isinstance(cache.cleanup_delay, float)
             assert cache.cleanup_delay >= 0.0
-            if __debug__: dprint("canceling timeout on ", identifier, " for ", cache)
+            if __debug__: dprint("canceling timeout on ", identifier_to_string(identifier), " for ", cache)
 
             if cache.cleanup_delay:
                 self._callback.replace_register("requestcache-%s" % identifier, self._on_cleanup, (identifier,), delay=cache.cleanup_delay)
@@ -81,7 +83,7 @@ class RequestCache(object):
     def _on_timeout(self, identifier):
         assert identifier in self._identifiers, identifier
         cache = self._identifiers.get(identifier)
-        if __debug__: dprint("timeout on ", identifier, " for ", cache)
+        if __debug__: dprint("timeout on ", identifier_to_string(identifier), " for ", cache)
         cache.on_timeout()
         if cache.cleanup_delay:
             self._callback.register(self._on_cleanup, (identifier,), id_="requestcache-%s" % identifier, delay=cache.cleanup_delay)
@@ -90,5 +92,5 @@ class RequestCache(object):
 
     def _on_cleanup(self, identifier):
         assert identifier in self._identifiers
-        if __debug__: dprint("cleanup on ", identifier, " for ", self._identifiers[identifier])
+        if __debug__: dprint("cleanup on ", identifier_to_string(identifier), " for ", self._identifiers[identifier])
         del self._identifiers[identifier]
