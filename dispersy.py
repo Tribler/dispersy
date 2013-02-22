e34e0bc34b6e740bfab1f10fdf9be29d5bf20aca
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2013-02-22 12:32:07 +0000

    git-svn-id: https://svn.tribler.org/dispersy/trunk@31637 001aeff7-3401-0410-a489-f7902fc005dd

--- dispersy.py
+++ dispersy.py
@@ -286,7 +286,6 @@ class Dispersy(Singleton):
         assert isinstance(callback, Callback)
         assert isinstance(working_directory, unicode)
         assert isinstance(database_filename, unicode)
-        assert False
 
         super(Dispersy, self).__init__()
 
@@ -2227,8 +2226,7 @@ ORDER BY global_time""", (meta.database_id, member_database_id)))
         assert wan_address == ("0.0.0.0", 0) or self.is_valid_address(wan_address), [self._wan_address, wan_address]
         
         #TEMP for das2 testing
-        #assert lan_address[0].split(".")[-1] == wan_address[0].split(".")[-1], [lan_address, wan_address]
-        
+        assert lan_address[0].split(".")[-1] == wan_address[0].split(".")[-1], [lan_address, wan_address]
         return lan_address, wan_address
 
     def take_step(self, community, allow_sync):
