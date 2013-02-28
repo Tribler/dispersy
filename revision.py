0e46536e85ac142dd139d0f42f85667614f48afa
Elric Milon <whirm@gmx.com>
2013-02-28 14:41:04 +0100

    Disable SVN revision code so it works on Git

--- revision.py
+++ revision.py
@@ -1,7 +1,9 @@
 _revision_information = {}
 
 def update_revision_information(url, revision):
-    _revision_information[url[10:-2]] = int(revision[11:-2])
+    #Disable until we adapt this to git.
+    pass
+    #_revision_information[url[10:-2]] = int(revision[11:-2])
 
 def get_revision_information():
     return _revision_information
