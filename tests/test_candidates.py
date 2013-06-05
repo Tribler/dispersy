2b221fe4d38fc74fe0da36df4f4885abbb27b4a3
Niels Zeilemaker <niels@zeilemaker.nl>
2013-06-05 16:16:16 +0200

    Extended the candidate unittests

--- tests/test_candidates.py
+++ tests/test_candidates.py
@@ -34,7 +34,6 @@ class TestCandidates(DispersyTestClass):
 
         self.assertEquals(expected, got)
 
-    @call_on_dispersy_thread
     def __test_introduce(self, community_create_method):
         c = community_create_method(self._dispersy, self._my_member)
         candidates = []
@@ -68,6 +67,66 @@ class TestCandidates(DispersyTestClass):
 
         self.assertEquals(expected, got)
         return [c, c2], candidates
+
+    @call_on_dispersy_thread
+    def test_introduction_probabilities(self):
+        c = DebugCommunity.create_community(self._dispersy, self._my_member)
+
+        candidates = []
+        for i in range(2):
+            address = ("127.0.0.1", i + 1)
+            candidate = c.create_candidate(address, False, address, address, u"unknown")
+            candidates.append(candidate)
+
+        # mark 1 candidate as walk, 1 as stumble
+        now = time()
+        candidates[0].walk(c, now, 10.5)
+        candidates[0].walk_response(c)
+        candidates[1].stumble(c, now)
+
+        # fetch candidates
+        returned_walked_candidate = 0
+        expected_walked_range = range(4500, 5500)
+        for i in xrange(10000):
+            candidate = c.dispersy_get_introduce_candidate()
+            returned_walked_candidate += 1 if candidate.sock_addr[1] == 1 else 0
+
+        assert returned_walked_candidate in expected_walked_range
+
+    @call_on_dispersy_thread
+    def test_walk_probabilities(self):
+        c = DebugCommunity.create_community(self._dispersy, self._my_member)
+
+        candidates = []
+        for i in range(3):
+            address = ("127.0.0.1", i + 1)
+            candidate = c.create_candidate(address, False, address, address, u"unknown")
+            candidates.append(candidate)
+
+        # mark 1 candidate as walk, 1 as stumble
+        now = time()
+        candidates[0].walk(c, now - CANDIDATE_ELIGIBLE_DELAY, 10.5)
+        candidates[0].walk_response(c)
+        candidates[1].stumble(c, now)
+        candidates[2].intro(c, now)
+
+        # fetch candidates
+        returned_walked_candidate = 0
+        expected_walked_range = range(4497, 5475)
+        returned_stumble_candidate = 0
+        expected_stumble_range = range(1975, 2975)
+        returned_intro_candidate = 0
+        expected_intro_range = range(1975, 2975)
+        for i in xrange(10000):
+            candidate = c.dispersy_get_walk_candidate()
+
+            returned_walked_candidate += 1 if candidate.sock_addr[1] == 1 else 0
+            returned_stumble_candidate += 1 if candidate.sock_addr[1] == 2 else 0
+            returned_intro_candidate += 1 if candidate.sock_addr[1] == 3 else 0
+
+        assert returned_walked_candidate in expected_walked_range, returned_walked_candidate
+        assert returned_stumble_candidate in expected_stumble_range, returned_stumble_candidate
+        assert returned_intro_candidate in expected_intro_range, returned_intro_candidate
 
     @call_on_dispersy_thread
     def test_merge_candidates(self):
