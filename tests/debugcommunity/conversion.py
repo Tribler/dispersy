9d5bf74fcee8544d591ac7f97ecf95cc62dd70c4
Boudewijn <boudewijn-tribler@frayja.com>
2013-06-28 11:10:40 +0200

    Better message numbers and version control.
    - The community version can now be specified.
    
    - The message numbers now all start at 100, allowing us to distinguish
      between dispersy messages (that start at 255 and go lower),
      community messages (that start at 1 and go higher), and debug
      messages (that start at 100 and go higher).

--- tests/debugcommunity/conversion.py
+++ tests/debugcommunity/conversion.py
@@ -9,19 +9,23 @@ class DebugCommunityConversion(BinaryConversion):
     """
     DebugCommunityConversion is used to convert messages to and from binary while performing unittests.
     """
-    def __init__(self, community):
-        super(DebugCommunityConversion, self).__init__(community, "\x02")
-        self.define_meta_message(chr(1), community.get_meta_message(u"last-1-test"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(2), community.get_meta_message(u"last-9-test"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(4), community.get_meta_message(u"double-signed-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(8), community.get_meta_message(u"full-sync-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(9), community.get_meta_message(u"ASC-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(10), community.get_meta_message(u"DESC-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(11), community.get_meta_message(u"last-1-doublemember-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(12), community.get_meta_message(u"protected-full-sync-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(13), community.get_meta_message(u"dynamic-resolution-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(14), community.get_meta_message(u"sequence-text"), self._encode_text, self._decode_text)
-        self.define_meta_message(chr(15), community.get_meta_message(u"full-sync-global-time-pruning-text"), self._encode_text, self._decode_text)
+    def __init__(self, community, version="\x02"):
+        assert isinstance(version, str), type(version)
+        assert len(version) == 1, len(version)
+        super(DebugCommunityConversion, self).__init__(community, version)
+        # we use higher message identifiers to reduce the chance that we clash with either Dispersy (255 and down) and
+        # normal communities (1 and up).
+        self.define_meta_message(chr(101), community.get_meta_message(u"last-1-test"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(102), community.get_meta_message(u"last-9-test"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(103), community.get_meta_message(u"double-signed-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(104), community.get_meta_message(u"full-sync-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(105), community.get_meta_message(u"ASC-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(106), community.get_meta_message(u"DESC-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(107), community.get_meta_message(u"last-1-doublemember-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(108), community.get_meta_message(u"protected-full-sync-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(109), community.get_meta_message(u"dynamic-resolution-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(110), community.get_meta_message(u"sequence-text"), self._encode_text, self._decode_text)
+        self.define_meta_message(chr(111), community.get_meta_message(u"full-sync-global-time-pruning-text"), self._encode_text, self._decode_text)
 
     def _encode_text(self, message):
         """
