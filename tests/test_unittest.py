952e3a7618429197b3bd4b779a644990baaeee13
Boudewijn <boudewijn-tribler@frayja.com>
2013-07-23 13:52:26 +0200

    Replaces expectFailure with failure_to_success and fixes running in optimized.

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,46 +1,57 @@
-from unittest import expectedFailure
 import logging
 logger = logging.getLogger(__name__)
 
 from .dispersytestclass import DispersyTestFunc, call_on_dispersy_thread
 
-class TestUnittestFunc(DispersyTestFunc):
+def failure_to_success(exception_class, exception_message):
+    def helper1(func):
+        def helper2(*args, **kargs):
+            try:
+                func(*args, **kargs)
+            except Exception as exception:
+                if isinstance(exception, exception_class) and exception.message == exception_message:
+                    return
+
+                # if isinstance(exception, KeyError) and exception.message == "This must fail":
+                #     return
+
+                # if isinstance(exception, AssertionError) and exception.message == "This must fail":
+                #     return
+
+                # not one of the pre-programmed exceptions, test should indicate failure
+                raise
+
+        helper2.__name__ = func.__name__
+        return helper2
+    return helper1
+
+class TestUnittest(DispersyTestFunc):
     """
     Tests ensuring that an exception anywhere in _dispersy.callback is propagated to the unittest framework.
     """
 
-    def addExpectedFailure(self, test, err):
-        if isinstance(err, KeyError) and err.message == "This must fail":
-            return super(TestUnittestFunc, self).addExpectedFailure(test, err)
-
-        if isinstance(err, AssertionError) and err.message == "This must fail":
-            return super(TestUnittestFunc, self).addExpectedFailure(test, err)
-
-        # unexpected error
-        return super(TestUnittestFunc, self).addError(test, err)
-
-    @expectedFailure
+    @failure_to_success(AssertionError, "This must fail")
     @call_on_dispersy_thread
     def test_assert(self):
         " Trivial assert. "
-        assert False, "This must fail"
+        self.assertTrue(False, "This must fail")
 
-    @expectedFailure
+    @failure_to_success(KeyError, "This must fail")
     @call_on_dispersy_thread
     def test_KeyError(self):
         " Trivial KeyError. "
         raise KeyError("This must fail")
 
-    @expectedFailure
+    @failure_to_success(AssertionError, "This must fail")
     @call_on_dispersy_thread
     def test_assert_callback(self):
         " Assert within a registered task. "
         def task():
-            assert False, "This must fail"
+            self.assertTrue(False, "This must fail")
         self._dispersy.callback.register(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(KeyError, "This must fail")
     @call_on_dispersy_thread
     def test_KeyError_callback(self):
         " KeyError within a registered task. "
@@ -49,18 +60,18 @@ class TestUnittestFunc(DispersyTestFunc):
         self._dispersy.callback.register(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(AssertionError, "This must fail")
     @call_on_dispersy_thread
     def test_assert_callback_generator(self):
         " Assert within a registered generator task. "
         def task():
             yield 0.1
             yield 0.1
-            assert False, "This must fail"
+            self.assertTrue(False, "This must fail")
         self._dispersy.callback.register(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(KeyError, "This must fail")
     @call_on_dispersy_thread
     def test_KeyError_callback_generator(self):
         " KeyError within a registered generator task. "
@@ -71,16 +82,16 @@ class TestUnittestFunc(DispersyTestFunc):
         self._dispersy.callback.register(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(AssertionError, "This must fail")
     @call_on_dispersy_thread
     def test_assert_callback_call(self):
         " Assert within a 'call' task. "
         def task():
-            assert False, "This must fail"
+            self.assertTrue(False, "This must fail")
         self._dispersy.callback.call(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(KeyError, "This must fail")
     @call_on_dispersy_thread
     def test_KeyError_callback_call(self):
         " KeyError within a 'call' task. "
@@ -89,18 +100,18 @@ class TestUnittestFunc(DispersyTestFunc):
         self._dispersy.callback.call(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(AssertionError, "This must fail")
     @call_on_dispersy_thread
     def test_assert_callback_call_generator(self):
         " Assert within a 'call' generator task. "
         def task():
             yield 0.1
             yield 0.1
-            assert False, "This must fail"
+            self.assertTrue(False, "This must fail")
         self._dispersy.callback.call(task)
         yield 10.0
 
-    @expectedFailure
+    @failure_to_success(KeyError, "This must fail")
     @call_on_dispersy_thread
     def test_KeyError_callback_call_generator(self):
         " KeyError within a 'call' generator task. "
