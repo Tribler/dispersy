ae4c1122a3985fa96048281750ed00895d70a402
Niels Zeilemaker <niels@zeilemaker.nl>
2013-07-23 10:31:31 +0200

    Renamed test + added doc

--- tests/test_nat_detection.py
+++ tests/test_nat_detection.py
@@ -1,10 +1,19 @@
 from .dispersytestclass import DispersyTestFunc, call_on_dispersy_thread
 from .debugcommunity.community import DebugCommunity
 
-class TestVoting(DispersyTestFunc):
+class TestNATDetection(DispersyTestFunc):
+    """
+    Tests NAT detection.
+
+    These unit tests should cover all methods which are related to detecting the NAT type of a peer.
+    """
 
     @call_on_dispersy_thread
     def test_symmetric_vote(self):
+        """
+        A receiving two votes from different candidates for different port numbers, a peer
+        must change it's connection type to summetric-NAT.
+        """
         c = DebugCommunity.create_community(self._dispersy, self._my_member)
 
         for i in range(2):
