11031ac5ca6577456909f6f6b46979a6b2899dd8
Niels Zeilemaker <niels@zeilemaker.nl>
2014-02-07 14:51:22 +0100

    Fix for now must be float

--- cache.py
+++ cache.py
@@ -65,7 +65,7 @@ class IntroductionRequestCache(NumberCache):
             self.community.dispersy.statistics.dict_inc(self.community.dispersy.statistics.walk_fail, self.helper_candidate.sock_addr)
 
             # set the walk repsonse to be invalid
-            self.helper_candidate.walk_response(-1)
+            self.helper_candidate.walk_response(-1.0)
 
     def _check_if_both_received(self):
         if self._introduction_response_received and self._puncture_received:
