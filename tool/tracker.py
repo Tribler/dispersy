c983464d616c9269833059e95b9291ddf5a074ac
Lipu Fei <lipu.fei815@gmail.com>
2014-05-20 12:32:59 +0200

    some fix

--- tool/tracker.py
+++ tool/tracker.py
@@ -340,7 +340,7 @@ class TrackerDispersy(Dispersy):
         for community in self._communities.itervalues():
             mapping[type(community)] += 1
 
-        print "BANDWIDTH", self._endpoint.total_up, self._endpoint.total_down
+        print "BANDWIDTH", self._statistics.total_up, self._statistics.total_down
         print "COMMUNITY", mapping[TrackerCommunity], mapping[TrackerHardKilledCommunity], mapping[DiscoveryCommunity]
         print "CANDIDATE2", sum(len(list(community.dispersy_yield_verified_candidates())) for community in self._communities.itervalues())
 
