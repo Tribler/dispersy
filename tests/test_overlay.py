6c98ae837d953936793d8962ef86aae63bb43ffa
Elric Milon <whirm@gmx.com>
2014-05-28 18:42:44 +0200

    add more statistics

--- tests/test_overlay.py
+++ tests/test_overlay.py
@@ -94,6 +94,8 @@ class TestOverlay(DispersyTestFunc):
             info.candidates = [(candidate, candidate.get_category(now)) for candidate in community._candidates.itervalues()]
             info.verified_candidates = [(candidate, candidate.get_category(now)) for candidate in community.dispersy_yield_verified_candidates()]
             info.incoming_walks = dispersy.statistics.incoming_intro_count
+            info.outgoing_intro_count = dispersy.statistics.outgoing_intro_count
+            info.walk_success_count = dispersy.statistics.walk_success_count
             info.lan_address = dispersy.lan_address
             info.wan_address = dispersy.wan_address
             info.connection_type = dispersy.connection_type
@@ -142,7 +144,7 @@ class TestOverlay(DispersyTestFunc):
 
         # write graph statistics
         with open("%s_connections.txt" % cid_hex, "w+") as handle:
-            handle.write("TIME VERIFIED_CANDIDATES WALK_CANDIDATES STUMBLE_CANDIDATES INTRO_CANDIDATES NONE_CANDIDATES INCOMING_WALKS LAN_ADDRESS WAN_ADDRESS CONNECTION_TYPE\n")
+            handle.write("TIME VERIFIED_CANDIDATES WALK_CANDIDATES STUMBLE_CANDIDATES INTRO_CANDIDATES NONE_CANDIDATES INCOMING_WALKS OUTGOING_WALKS WALK_SUCCESS LAN_ADDRESS WAN_ADDRESS CONNECTION_TYPE\n")
             for info in history:
                 handle.write("%f   %d   %d   %d   %d   %d   %d   %s   %s   \"%s\"\n" % (
                         info.diff,
@@ -152,6 +154,8 @@ class TestOverlay(DispersyTestFunc):
                         len([_ for _, category in info.candidates if category == u"intro"]),
                         len([_ for _, category in info.candidates if category is None]),
                         info.incoming_walks,
+                        info.outgoing_intro_count,
+                        info.walk_success_count,
                         "%s:%d" % info.lan_address,
                         "%s:%d" % info.wan_address,
                         info.connection_type))
