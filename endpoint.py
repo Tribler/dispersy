6a7de73f91bb3a78ce51c9a573b23b8ed1369f9f
Niels Zeilemaker <nielszeilemaker@godatadriven.com>
2015-06-16 18:34:26 +0200

    Removed all references to RawServerEndpoint, implemented a limit on the sendqueue. Will drop all messages older than 300s.

--- endpoint.py
+++ endpoint.py
@@ -22,15 +22,6 @@ TUNNEL_PREFIX = "ffffffff".decode("HEX")
 TUNNEL_PREFIX_LENGHT = 4
 
 
-def strip_if_tunnel(datagram):
-    """
-    Returns is_tunnel, prefix_stripped_datagram
-    """
-    if datagram.startswith(TUNNEL_PREFIX):
-        return True, datagram[TUNNEL_PREFIX_LENGHT:]
-    return False, datagram
-
-
 class Endpoint(object):
     __metaclass__ = ABCMeta
 
@@ -103,19 +94,20 @@ class NullEndpoint(Endpoint):
         self._dispersy.statistics.total_up += len(packet)
 
 
-class RawserverEndpoint(Endpoint):
+class StandaloneEndpoint(Endpoint):
 
-    def __init__(self, rawserver, port, ip="0.0.0.0"):
-        super(RawserverEndpoint, self).__init__()
+    def __init__(self, port, ip="0.0.0.0"):
+        super(StandaloneEndpoint, self).__init__()
 
-        self._rawserver = rawserver
         self._port = port
         self._ip = ip
-        self._add_task = self._rawserver.add_task
+        self._running = False
+        self._add_task = lambda task, delay = 0.0, id = "": None
         self._sendqueue_lock = threading.RLock()
         self._sendqueue = []
 
-        # _SOCKET is set during open(...)
+        # _THREAD and _THREAD are set during open(...)
+        self._thread = None
         self._socket = None
         self.packet_handlers = {}
 
@@ -125,27 +117,94 @@ class RawserverEndpoint(Endpoint):
     def stop_listen_to(self, prefix):
         del self.packet_handlers[prefix]
 
-    def open(self, dispersy):
-        super(RawserverEndpoint, self).open(dispersy)
+    def get_address(self):
+        assert self._dispersy, "Should not be called before open(...)"
+        return self._socket.getsockname()
 
-        while True:
+    def open(self, dispersy):
+        super(StandaloneEndpoint, self).open(dispersy)
+
+        for _ in xrange(10000):
             try:
-                self._socket = self._rawserver.create_udpsocket(self._port, self._ip)
                 self._logger.debug("Listening at %d", self._port)
+                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 870400)
+                self._socket.bind((self._ip, self._port))
+                self._socket.setblocking(0)
+
+                self._port = self._socket.getsockname()[1]
             except socket.error:
                 self._port += 1
                 continue
             break
-        self._rawserver.start_listening_udp(self._socket, self)
+
+        self._running = True
+        self._thread = threading.Thread(name="StandaloneEndpoint", target=self._loop)
+        self._thread.daemon = True
+        self._thread.start()
         return True
 
-    def close(self, timeout=0.0):
-        self._rawserver.stop_listening_udp(self._socket)
-        return super(RawserverEndpoint, self).close(timeout)
+    def close(self, timeout=10.0):
+        self._running = False
+        result = True
 
-    def get_address(self):
+        if timeout > 0.0:
+            self._thread.join(timeout)
+
+            if self._thread.is_alive():
+                self._logger.error("the endpoint thread is still running (after waiting %f seconds)", timeout)
+                result = False
+
+        else:
+            if self._thread.is_alive():
+                self._logger.debug("the endpoint thread is still running (use timeout > 0.0 to ensure the thread stops)")
+                result = False
+
+        try:
+            self._socket.close()
+        except socket.error as exception:
+            self._logger.exception("%s", exception)
+            result = False
+
+        return super(StandaloneEndpoint, self).close(timeout) and result
+
+    def _loop(self):
         assert self._dispersy, "Should not be called before open(...)"
-        return self._socket.getsockname()
+        recvfrom = self._socket.recvfrom
+        socket_list = [self._socket.fileno()]
+
+        prev_sendqueue = 0
+        while self._running:
+            # This is a tricky, if we are running on the DAS4 whenever a socket is ready for writing all processes of
+            # this node will try to write. Therefore, we have to limit the frequency of trying to write a bit.
+            if self._sendqueue and (time() - prev_sendqueue) > 0.1:
+                read_list, write_list, _ = select(socket_list, socket_list, [], 0.1)
+            else:
+                read_list, write_list, _ = select(socket_list, [], [], 0.1)
+
+            # Furthermore, if we are allowed to send, process sendqueue immediately
+            if write_list:
+                self._process_sendqueue()
+                prev_sendqueue = time()
+
+            if read_list:
+                packets = []
+                try:
+                    while True:
+                        (data, sock_addr) = recvfrom(65535)
+                        if data:
+                            packets.append((sock_addr, data))
+                        else:
+                            break
+
+                except socket.error as e:
+                    if e.errno != errno.EAGAIN:
+                        self._dispersy.statistics.dict_inc(u"endpoint_recv", u"socket-error-'%s'" % str(e))
+
+                finally:
+                    if packets:
+                        self._logger.debug('%d came in, %d bytes in total', len(packets), sum(len(packet) for _, packet in packets))
+                        self.data_came_in(packets)
 
     def data_came_in(self, packets, cache=True):
         assert self._dispersy, "Should not be called before open(...)"
@@ -154,7 +213,6 @@ class RawserverEndpoint(Endpoint):
 
         normal_packets = []
         for packet in packets:
-
             prefix = next((p for p in self.packet_handlers if
                            packet[1].startswith(p)), None)
             if prefix:
@@ -163,8 +221,6 @@ class RawserverEndpoint(Endpoint):
             else:
                 normal_packets.append(packet)
 
-        # the rawserver SUCKS.  every now and then exceptions are not shown and apparently we are
-        # sometimes called without any packets...
         if normal_packets:
             self._dispersy.statistics.total_down += sum(len(data) for _, data in normal_packets)
             if self._logger.isEnabledFor(logging.DEBUG):
@@ -176,13 +232,20 @@ class RawserverEndpoint(Endpoint):
 
     def dispersythread_data_came_in(self, packets, timestamp, cache=True):
         assert self._dispersy, "Should not be called before open(...)"
-        iterator = ((data.startswith(TUNNEL_PREFIX), sock_addr, data) for sock_addr, data in packets)
+
+        def strip_if_tunnel(packets):
+            for sock_addr, data in packets:
+                if data.startswith(TUNNEL_PREFIX):
+                    yield True, sock_addr, data[TUNNEL_PREFIX_LENGHT:]
+                else:
+                    yield False, sock_addr, data
+
         self._dispersy.on_incoming_packets([(Candidate(sock_addr, tunnel), data[4:] if tunnel else data)
                                             for tunnel, sock_addr, data
-                                            in iterator],
+                                            in strip_if_tunnel(packets)],
                                            cache,
                                            timestamp,
-                                           u"rawserver")
+                                           u"standalone_ep")
 
     def send(self, candidates, packets, prefix=None):
         assert self._dispersy, "Should not be called before open(...)"
@@ -230,7 +293,7 @@ class RawserverEndpoint(Endpoint):
         except socket.error:
             with self._sendqueue_lock:
                 did_have_senqueue = bool(self._sendqueue)
-                self._sendqueue.append((candidate.sock_addr, data))
+                self._sendqueue.append((time(), candidate.sock_addr, data))
 
             # If we did not have a sendqueue, then we need to call process_sendqueue in order send these messages
             if not did_have_senqueue:
@@ -247,21 +310,27 @@ class RawserverEndpoint(Endpoint):
                 self._logger.debug("%d left in sendqueue, trying to send %d packets",
                                    len(self._sendqueue), NUM_PACKETS)
 
+                allowed_timestamp = time() - 300
+
                 for i in xrange(NUM_PACKETS):
-                    sock_addr, data = self._sendqueue[i]
-                    try:
-                        self._socket.sendto(data, sock_addr)
+                    queued_at, sock_addr, data = self._sendqueue[i]
+                    if queued_at > allowed_timestamp:
+                        try:
+                            self._socket.sendto(data, sock_addr)
+                            index += 1
+
+                            if self._logger.isEnabledFor(logging.DEBUG):
+                                self.log_packet(sock_addr, data)
+
+                        except socket.error as e:
+                            if e[0] != SOCKET_BLOCK_ERRORCODE:
+                                self._logger.warning("could not send %d to %s (%d in sendqueue)",
+                                                     len(data), sock_addr, len(self._sendqueue))
+                                self._dispersy.statistics.dict_inc(u"endpoint_send", u"socket-error")
+                            break
+                    else:
+                        self._dispersy.statistics.dict_inc(u"endpoint_send", u"packet-expired")
                         index += 1
-
-                        if self._logger.isEnabledFor(logging.DEBUG):
-                            self.log_packet(sock_addr, data)
-
-                    except socket.error as e:
-                        if e[0] != SOCKET_BLOCK_ERRORCODE:
-                            self._logger.warning("could not send %d to %s (%d in sendqueue)",
-                                                 len(data), sock_addr, len(self._sendqueue))
-                            self._dispersy.statistics.dict_inc(u"endpoint_send", u"socket-error")
-                        break
 
                 self._sendqueue = self._sendqueue[index:]
                 if self._sendqueue:
@@ -271,110 +340,6 @@ class RawserverEndpoint(Endpoint):
 
                 self._dispersy.statistics.cur_sendqueue = len(self._sendqueue)
 
-
-class StandaloneEndpoint(RawserverEndpoint):
-
-    def __init__(self, port, ip="0.0.0.0"):
-        # do NOT call RawserverEndpoint.__init__!
-        Endpoint.__init__(self)
-
-        self._port = port
-        self._ip = ip
-        self._running = False
-        self._add_task = lambda task, delay = 0.0, id = "": None
-        self._sendqueue_lock = threading.RLock()
-        self._sendqueue = []
-
-        # _THREAD and _THREAD are set during open(...)
-        self._thread = None
-        self._socket = None
-        self.packet_handlers = {}
-
-    def open(self, dispersy):
-        # do NOT call RawserverEndpoint.open!
-        Endpoint.open(self, dispersy)
-        for _ in xrange(10000):
-            try:
-                self._logger.debug("Listening at %d", self._port)
-                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 870400)
-                self._socket.bind((self._ip, self._port))
-                self._socket.setblocking(0)
-
-                self._port = self._socket.getsockname()[1]
-            except socket.error:
-                self._port += 1
-                continue
-            break
-
-        self._running = True
-        self._thread = threading.Thread(name="StandaloneEndpoint", target=self._loop)
-        self._thread.daemon = True
-        self._thread.start()
-        return True
-
-    def close(self, timeout=10.0):
-        self._running = False
-        result = True
-
-        if timeout > 0.0:
-            self._thread.join(timeout)
-
-            if self._thread.is_alive():
-                self._logger.error("the endpoint thread is still running (after waiting %f seconds)", timeout)
-                result = False
-
-        else:
-            if self._thread.is_alive():
-                self._logger.debug("the endpoint thread is still running (use timeout > 0.0 to ensure the thread stops)")
-                result = False
-
-        try:
-            self._socket.close()
-        except socket.error as exception:
-            self._logger.exception("%s", exception)
-            result = False
-
-        # do NOT call RawserverEndpoint.open!
-        return Endpoint.close(self, timeout) and result
-
-    def _loop(self):
-        assert self._dispersy, "Should not be called before open(...)"
-        recvfrom = self._socket.recvfrom
-        socket_list = [self._socket.fileno()]
-
-        prev_sendqueue = 0
-        while self._running:
-            # This is a tricky, if we are running on the DAS4 whenever a socket is ready for writing all processes of
-            # this node will try to write. Therefore, we have to limit the frequency of trying to write a bit.
-            if self._sendqueue and (time() - prev_sendqueue) > 0.1:
-                read_list, write_list, _ = select(socket_list, socket_list, [], 0.1)
-            else:
-                read_list, write_list, _ = select(socket_list, [], [], 0.1)
-
-            # Furthermore, if we are allowed to send, process sendqueue immediately
-            if write_list:
-                self._process_sendqueue()
-                prev_sendqueue = time()
-
-            if read_list:
-                packets = []
-                try:
-                    while True:
-                        (data, sock_addr) = recvfrom(65535)
-                        if data:
-                            packets.append((sock_addr, data))
-                        else:
-                            break
-
-                except socket.error as e:
-                    if e.errno != errno.EAGAIN:
-                        self._dispersy.statistics.dict_inc(u"endpoint_recv", u"socket-error-'%s'" % str(e))
-
-                finally:
-                    if packets:
-                        self._logger.debug('%d came in, %d bytes in total', len(packets), sum(len(packet) for _, packet in packets))
-                        self.data_came_in(packets)
 
 class ManualEnpoint(StandaloneEndpoint):
 
