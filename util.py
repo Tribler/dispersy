e114ce4b3281a5893634c26903038d491b7e39ee
Paul Brussee <brussee@live.nl>
2016-07-04 17:12:38 +0200

    log exception type

--- util.py
+++ util.py
@@ -234,8 +234,8 @@ def blockingCallFromThread(reactor, f, *args, **kwargs):
         if isinstance(result, failure.Failure):
             other_thread_tb = traceback.extract_tb(result.getTracebackObject())
             this_thread_tb = traceback.extract_stack()
-            logger.error("Exception raised on the reactor's thread '%s'.\n Traceback from this thread:\n%s\n"
-                         " Traceback from the reactor's thread:\n %s", result.getErrorMessage(),
+            logger.error("Exception raised on the reactor's thread %s: \"%s\".\n Traceback from this thread:\n%s\n"
+                         " Traceback from the reactor's thread:\n %s", result.type.__name__, result.getErrorMessage(),
                          ''.join(traceback.format_list(this_thread_tb)), ''.join(traceback.format_list(other_thread_tb)))
             result.raiseException()
         return result
